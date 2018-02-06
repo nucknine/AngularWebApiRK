117ce7d0ce5d4e4e4166d63c6f0fff5141e2d100
nucknine <nucknine@gmail.com>
2018-02-06 14:47:23 +0300

    companies profile

--- WebClient/app/managementCompanies/managementCompanyEditCtrl.js
+++ WebClient/app/managementCompanies/managementCompanyEditCtrl.js
@@ -11,76 +11,70 @@
         vm.managementCompany = {};
         vm.message = '';
         vm.title = '';
+        vm.email = currentUser.getUsername();
         vm.id = 0;
 
 
+        //get company by email
+        //if (vm.email) {
+        //    managementCompaniesResource.query(
+       
+        //        {
+        //            $filter: "substringof(Email, '" + vm.email + "')"
+        //        },
+        //        function (data) {
 
-        vm.select = function () {
-            managementCompaniesResource.get({ id: vm.id },
-                function (data) {
-                    vm.managementCompany = data;
-                    vm.originalmanagementCompany = angular.copy(data);
+        //            vm.habitant.habitantId = data[0].habitantId * 1;
+        //            habitantsResource.get({ id: vm.habitant.habitantId },
+        //                function (data) {
+        //                    vm.habitant = data;
+        //                    vm.originalHabitant = angular.copy(data);
+        //                    vm.title = "Edit: " + vm.habitant.name + ' ' + vm.habitant.surname;
+        //                }
+        //            );
 
-                    if (vm.managementCompany.managementCompanyId && vm.managementCompany.name) {
-                        vm.title = "Edit: " + vm.managementCompany.name;
-                    }
-                    else {
-                        vm.title = "New Management Company";
-                    }
-                },
-                function (response) {
-                    vm.message = response.statusText + "\r\n";
-                    if (response.data.exceptionMessage)
-                        vm.message += response.data.exceptionMessage;
-                });
+        //        },
+        //        function (response) {
+        //            console.log('response query by email' + response);
+        //        }
+        //    );
 
-        };
+        //    if (!vm.habitant[0]) {
+        //        vm.title = "New Habitant";
+        //        habitantsResource.get({ id: 0 },
+        //            function (data) {
+        //                vm.habitant = data;
+        //                vm.habitant.email = vm.email;
+        //                vm.title = "New Habitant";
+        //                vm.originalHabitant = angular.copy(data);
+        //            }
+        //        );
+        //    }
+        //}
+
+        managementCompaniesResource.get({ id: vm.id },
+            function (data) {
+                vm.managementCompany = data;
+                vm.originalmanagementCompany = angular.copy(data);
+
+                if (vm.managementCompany.managementCompanyId && vm.managementCompany.name) {
+                    vm.title = "Edit: " + vm.managementCompany.name;
+                }
+                else {
+                    vm.title = "New Management Company";
+                }
+            },
+            function (response) {
+                vm.message = response.statusText + "\r\n";
+                if (response.data.exceptionMessage)
+                    vm.message += response.data.exceptionMessage;
+            });
+
+        
 
 
 
-        vm.submit = function () {
-            vm.message = '';
-            if (vm.managementCompany.managementCompanyId) {
-                vm.managementCompany.$update({ id: vm.managementCompany.managementCompanyId },
-                    function (data) {
-                        vm.message = "... Save Complete";
-                    },
-                    function (response) {
-                        vm.message = response.statusText + "\r\n";
-                        if (response.data.modelState) {
-                            for (var key in response.data.modelState) {
-                                vm.message += response.data.modelState[key] + "\r\n";
-                            }
-                        }
-                        if (response.data.exceptionMessage)
-                            vm.message += response.data.exceptionMessage;
-                    });
-            }
-            else {
-                vm.managementCompany.$save(
-                    function (data) {
-                        vm.originalManagementCompany = angular.copy(data);
-
-                        vm.message = "... Save Complete";
-                    },
-                    function (response) {
-                        vm.message = response.statusText + "\r\n";
-                        if (response.data.modelState) {
-                            for (var key in response.data.modelState) {
-                                vm.message += response.data.modelState[key] + "\r\n";
-                            }
-                        }
-                        if (response.data.exceptionMessage)
-                            vm.message += response.data.exceptionMessage;
-                    });
-            }
-        };
-
-        vm.cancel = function (editForm) {
-            editForm.$setPristine();
-            vm.managementCompany = angular.copy(vm.originalManagementCompany);
-            vm.message = "";
-        };
+        
 
     }
 }());
--- WebClient/app/managementCompanies/managementCompanyListCtrl.js
+++ WebClient/app/managementCompanies/managementCompanyListCtrl.js
@@ -7,41 +7,111 @@
 
     function managementCompanyListCtrl(managementCompaniesResource, currentUser) {
         var vm = this;
-        vm.managementCompanyHomes = [];        
+        vm.managementCompanyHomes = [];
         vm.managementCompany = {};
         vm.email = currentUser.getUsername();
+        vm.managementCompany.managementCompanyId = 0;
+
+
+
 
         managementCompaniesResource.query({
             $filter: "substringof(Email, '" + vm.email + "')"
         },
             function (data) {
-                vm.managementCompany = data[0];
-                vm.managementCompany.managementCompanyId = data[0].managementCompanyId * 1;
+
+                if (data[0]) {
+                    vm.managementCompany = data[0];
+                    vm.managementCompany.managementCompanyId = data[0].managementCompanyId * 1;
+                    vm.originalmanagementCompany = angular.copy(data[0]);
+                    vm.title = "Edit: " + vm.managementCompany.name;
+                }
+                
 
 
                 if (vm.managementCompany.managementCompanyId) {
-                managementCompaniesResource.findHomes({ id: vm.managementCompany.managementCompanyId, flag: true },
-                    function (data) {
-                        vm.managementCompanyHomes.push(data);
-                    },
-                    function (response) {
+                    managementCompaniesResource.findHomes({ id: vm.managementCompany.managementCompanyId, flag: true },
+                        function (data) {
+                            vm.managementCompanyHomes.push(data);
+                        },
+                        function (response) {
 
-                        vm.message = response + "\r\n";
-                        if (response.data.exceptionMessage)
-                            vm.message += response.data.exceptionMessage;
-                    });
+                            vm.message = response + "\r\n";
+                            if (response.data.exceptionMessage)
+                                vm.message += response.data.exceptionMessage;
+                        });
+
+
                 }
+
 
 
             });
 
-        
+        if (!vm.managementCompany.managementCompanyId) {
+            managementCompaniesResource.get({ id: 0 },
+                function (data) {
+                    console.log("data " + data);
+                    vm.managementCompany = data;
+                    
+                    vm.managementCompany.email = currentUser.getUsername();
+                    
+                    vm.title = "New Company";
+                    vm.originalmanagementCompany = angular.copy(data);
+                }
+            );
+        }
 
-       
+        //Form
 
-        
+        vm.submit = function () {
+            vm.message = '';
+            if (vm.managementCompany.managementCompanyId) {
+                vm.managementCompany.$update({ id: vm.managementCompany.managementCompanyId },
+                    function (data) {
+                        vm.message = "... Save Complete";
+                    },
+                    function (response) {
+                        vm.message = response.statusText + "\r\n";
+                        if (response.data.modelState) {
+                            for (var key in response.data.modelState) {
+                                vm.message += response.data.modelState[key] + "\r\n";
+                            }
+                        }
+                        if (response.data.exceptionMessage)
+                            vm.message += response.data.exceptionMessage;
+                    });
+            }
+            else {
+                vm.managementCompany.$save(
+                    function (data) {
+                        vm.originalManagementCompany = angular.copy(data);
 
-        
+                        vm.message = "... Save Complete";
+                    },
+                    function (response) {
+                        vm.message = response.statusText + "\r\n";
+                        if (response.data.modelState) {
+                            for (var key in response.data.modelState) {
+                                vm.message += response.data.modelState[key] + "\r\n";
+                            }
+                        }
+                        if (response.data.exceptionMessage)
+                            vm.message += response.data.exceptionMessage;
+                    });
+            }
+        };
+
+        vm.cancel = function (editForm) {
+            editForm.$setPristine();
+            vm.managementCompany = angular.copy(vm.originalManagementCompany);
+            vm.message = "";
+        };
+
+
+
+
+
 
 
         //List of all companies
